624489e7f6fc9c5e83d63f8ce6b454acc7ad071b
1301yashraj <1301yashraj@gmail.com>
2025-09-30 20:57:04 +0530

    COnfiguration server working setup with configuration Folder

--- src/main/java/raj/yash/licenses/Application.java
+++ src/main/java/raj/yash/licenses/Application.java
@@ -1,9 +1,11 @@
-package main.java.raj.yash.licenses;
+package raj.yash.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
